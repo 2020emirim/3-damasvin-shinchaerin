b0e443d8192606e816d1466982c79003285f9407
s2018w33 <s2018w33@e-mirim.hs.kr>
2020-07-03 15:17:14 +0900

    startapp order

--- menu/views.py
+++ menu/views.py
@@ -8,6 +8,7 @@ from menu.models import Drink, Coffee, Bubbletea
 class DrinkListView(ListView):
     model = Drink
 
+
 class CoffeeCreateView(CreateView):
     model = Coffee
     fields = '__all__' # ['category', 'name', 'price', 'image']
@@ -15,17 +16,21 @@ class CoffeeCreateView(CreateView):
     success_url = reverse_lazy('menu:list')
     initial = {'category': 'Coffee'}
 
+
 class BubbleteaCreateView(CreateView):
     model = Bubbletea
     fields = '__all__' # ['category', 'name', 'price', 'image']
     template_name = 'menu/drink_create.html'
     success_url = reverse_lazy('menu:list')
     initial = {'category': 'Bubbletea'}
+
+
 class DrinkUpdateView(UpdateView):
     model = Drink
     fields = '__all__'
     template_name_suffix = '_update'    # bookmark_update.html
     success_url = reverse_lazy('menu:list')
+
 
 class DrinkDeleteView(DeleteView):
     model = Drink
