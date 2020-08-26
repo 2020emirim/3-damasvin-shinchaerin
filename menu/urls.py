ffa9199fbed22950b8bf22c67d568c9320720030
s2018w33 <s2018w33@e-mirim.hs.kr>
2020-08-26 22:26:41 +0900

    Re-Damasvin 완성

--- menu/urls.py
+++ menu/urls.py
@@ -5,9 +5,9 @@ from menu.views import DrinkListView, CoffeeCreateView, BubbleteaCreateView, Dri
 app_name = 'menu'
 
 urlpatterns = [
-    path('', DrinkListView.as_view(), name='list'),
-    path('add_coffee/', CoffeeCreateView.as_view(), name='add_coffee'),
-    path('add_bubbletea/', BubbleteaCreateView.as_view(), name='add_bubbletea'),
-    path('update/<int:pk>/', DrinkUpdateView.as_view(), name='update'),
-    path('delete/<int:pk>/', DrinkDeleteView.as_view(), name='delete'),
+  path('', DrinkListView.as_view(), name='list'),
+  path('add_coffee/', CoffeeCreateView.as_view(), name='add_coffee'),
+  path('add_bubbletea/', BubbleteaCreateView.as_view(), name='add_bubbletea'),
+  path('update/<int:pk>/', DrinkUpdateView.as_view(), name='update'),
+  path('delete/<int:pk>/', DrinkDeleteView.as_view(), name='delete'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -6,34 +6,34 @@ from menu.models import Drink, Coffee, Bubbletea
 
 
 class DrinkListView(ListView):
-    model = Drink
+  model = Drink
 
 
 class CoffeeCreateView(CreateView):
-    model = Coffee
-    fields = '__all__' # ['category', 'name', 'price', 'image']
-    template_name = 'menu/drink_create.html'
-    success_url = reverse_lazy('menu:list')
-    initial = {'category': 'Coffee'}
+  model = Coffee
+  fields = '__all__'  # ['category', 'name', 'price', 'image']
+  template_name = 'menu/drink_create.html'
+  success_url = reverse_lazy('menu:list')
+
+  initial = {'category': 'Coffee'}
 
 
 class BubbleteaCreateView(CreateView):
-    model = Bubbletea
-    fields = '__all__' # ['category', 'name', 'price', 'image']
-    template_name = 'menu/drink_create.html'
-    success_url = reverse_lazy('menu:list')
-    initial = {'category': 'Bubbletea'}
+  model = Bubbletea
+  fields = '__all__'  # ['category', 'name', 'price', 'image']
+  template_name = 'menu/drink_create.html'
+  success_url = reverse_lazy('menu:list')
+
+  initial = {'category': 'Bubbletea'}
 
 
 class DrinkUpdateView(UpdateView):
-    model = Drink
-    fields = '__all__'
-    template_name_suffix = '_update'    # bookmark_update.html
-    success_url = reverse_lazy('menu:list')
+  model = Drink
+  fields = '__all__'
+  template_name_suffix = '_update'
+  success_url = reverse_lazy('menu:list')
 
 
 class DrinkDeleteView(DeleteView):
-    model = Drink
-    success_url = reverse_lazy('menu:list')
-
-
+  model = Drink
+  success_url = reverse_lazy('menu:list')
